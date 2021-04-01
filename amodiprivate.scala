2e2cff4a32bbec793fb6a432a22c2de069532416
KD <kd@gmail.com>
2021-04-01 13:00:35 +0530

    updated

---
+++ amodiprivate.scala
@@ -0,0 +1,17 @@
+class abc
+{
+  private var a:Int = 123
+  def display()
+  {
+    a = 8
+    println(a)
+  }
+}
+
+object access extends App
+{
+  // class abc is accessible
+  // because this is in the same enclosing scope
+  var e = new abc()
+  e.display()
+}
